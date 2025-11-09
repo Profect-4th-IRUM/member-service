caab80fe51e5158c96e6d381f875bad6603d4e95
willjsw <sunwoo1137@gmail.com>
2025-11-09 15:44:55 +0900

    refactor #5: TestConfig 공통 모듈 config import, JPA Auditing 옵션 SpringBootApplication 단 활성화

--- src/main/java/com/irum/memberservice/MemberServiceApplication.java
+++ src/main/java/com/irum/memberservice/MemberServiceApplication.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableFeignClients(basePackages = "openfeign")
-@ComponentScan(basePackages = "com.irum")
+@EnableJpaAuditing
 @EnableDiscoveryClient
 public class MemberServiceApplication {
 
--- src/test/java/com/irum/memberservice/global/config/TestConfig.java
+++ src/test/java/com/irum/memberservice/global/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.irum.memberservice.global.config;
 
+import com.irum.global.infrastructure.config.GlobalAutoConfiguration;
 import com.irum.memberservice.domain.auth.service.AuthService;
 import com.irum.memberservice.domain.auth.service.JwtTokenService;
 import com.irum.memberservice.domain.member.service.ManagerService;
@@ -8,8 +9,10 @@ import com.irum.memberservice.global.util.CookieUtil;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 
 @TestConfiguration
+@Import(GlobalAutoConfiguration.class)
 public class TestConfig {
     @Bean
     public MemberService memberService() {
