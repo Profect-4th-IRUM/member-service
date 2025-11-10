3b90714d51a6496dc425c167d511fa0cb4119ea7
willjsw <willjsw@github.com>
2025-11-10 09:47:04 +0900

    refactor #8: open feign client 0.0.4 적용

--- src/main/java/com/irum/memberservice/domain/member/Internal/controller/MemberFeignController.java
+++ src/main/java/com/irum/memberservice/domain/member/Internal/controller/MemberFeignController.java
@@ -1,11 +1,11 @@
 package com.irum.memberservice.domain.member.Internal.controller;
 
 import com.irum.memberservice.domain.member.Internal.service.MemberInternalService;
+import com.irum.openfeign.member.dto.request.MemberIdListDto;
+import com.irum.openfeign.member.dto.response.MemberDto;
 import jakarta.validation.Valid;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import openfeign.member.dto.request.MemberIdListDto;
-import openfeign.member.dto.response.MemberDto;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
