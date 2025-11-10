d3808d1720c2ddcb23e1d63184f441816ba09dfe
willjsw <willjsw@github.com>
2025-11-10 13:40:03 +0900

    feature #10: Spring Security 인증 필터체인 제거 및 JWTAuth 필터 제거

--- src/main/java/com/irum/memberservice/domain/member/util/MemberValidator.java
+++ src/main/java/com/irum/memberservice/domain/member/util/MemberValidator.java
@@ -5,13 +5,9 @@ import com.irum.global.advice.exception.errorcode.GlobalErrorCode;
 import com.irum.memberservice.domain.member.domain.entity.Member;
 import com.irum.memberservice.domain.member.domain.entity.enums.Role;
 import com.irum.memberservice.domain.member.domain.repository.MemberRepository;
-import com.irum.memberservice.global.exception.errorcode.AuthErrorCode;
 import com.irum.memberservice.global.exception.errorcode.MemberErrorCode;
-import com.irum.memberservice.global.security.MemberDetails;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
@@ -21,31 +17,6 @@ import org.springframework.util.StringUtils;
 public class MemberValidator {
     private final MemberRepository memberRepository;
     private final BCryptPasswordEncoder passwordEncoder;
-
-    /** MemberUtil 구현된 메서드 사용(25.10.23~) * */
-    @Deprecated
-    public Member getCurrentMember() {
-        return memberRepository
-                .findByMemberId(getCurrentMemberId())
-                .orElseThrow(() -> new CommonException(MemberErrorCode.MEMBER_NOT_FOUND));
-    }
-
-    /** MemberUtil 구현된 메서드 사용(25.10.23~) * */
-    @Deprecated
-    private Long getCurrentMemberId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication == null || authentication.getPrincipal() == null) {
-            throw new CommonException(AuthErrorCode.AUTHENTICATION_NOT_FOUND);
-        }
-        try {
-            MemberDetails memberDetails = (MemberDetails) authentication.getPrincipal();
-            return memberDetails.getUserId();
-        } catch (ClassCastException e) {
-            throw new CommonException(AuthErrorCode.AUTHENTICATION_NOT_FOUND);
-        } catch (Exception e) {
-            throw new CommonException(AuthErrorCode.AUTHENTICATION_NOT_FOUND);
-        }
-    }
 
     public Member getMemberById(Long memberId) {
         return memberRepository
