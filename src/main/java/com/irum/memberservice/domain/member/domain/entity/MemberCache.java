49f94280967773930e999786a703a04234dd07d2
willjsw <sunwoo1137@gmail.com>
2025-11-17 16:21:01 +0900

    feature #24: Read-> Look Aside 캐싱 전략 구현

--- src/main/java/com/irum/memberservice/domain/member/domain/entity/MemberCache.java
+++ src/main/java/com/irum/memberservice/domain/member/domain/entity/MemberCache.java
@@ -1,32 +1,32 @@
 package com.irum.memberservice.domain.member.domain.entity;
 
-import com.irum.memberservice.domain.member.domain.entity.enums.Role;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
 @Getter
 @RedisHash(value = "member")
+@NoArgsConstructor
 public class MemberCache {
-    @Id
-    private Long memberId;
-    private String email;
-    private String name;
-    private String contact;
-    private Role role;
 
-    @TimeToLive
-    private long ttl;
+    @Id private Long memberId;
 
-    @Builder
-    private MemberCache(Long memberId, String email, String name, String contact, Role role, long ttl) {
+    private Member member;
+
+    @TimeToLive private long ttl;
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private MemberCache(Long memberId, Member member, long ttl) {
         this.memberId = memberId;
-        this.email = email;
-        this.name = name;
-        this.contact = contact;
-        this.role = role;
+        this.member = member;
         this.ttl = ttl;
+    }
+
+    public static MemberCache create(Long memberId, Member member, long ttl) {
+        return MemberCache.builder().memberId(memberId).member(member).ttl(ttl).build();
     }
 }
