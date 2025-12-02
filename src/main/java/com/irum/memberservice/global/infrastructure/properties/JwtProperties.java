fb748e321d1f7255488e01b4d1332c37079a7358
Bal1oon <ws32159@naver.com>
2025-12-02 17:08:57 +0900

    refactor #38: JWT 서명 알고리즘 변경

--- src/main/java/com/irum/memberservice/global/infrastructure/properties/JwtProperties.java
+++ src/main/java/com/irum/memberservice/global/infrastructure/properties/JwtProperties.java
@@ -4,11 +4,13 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "jwt")
 public record JwtProperties(
-        String accessTokenSecret,
+        String accessTokenPrivateKey,
+        String accessTokenPublicKey,
         String refreshTokenSecret,
         Long accessTokenExpirationTime,
         Long refreshTokenExpirationTime,
         String issuer) {
+
     public Long accessTokenExpirationMilliTime() {
         return accessTokenExpirationTime * 1000;
     }
